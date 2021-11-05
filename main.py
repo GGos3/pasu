2960438a83635fdf2613e1f8590d0030423b0a13
JangFeel <janpll0421@gmail.com>
2021-11-05 19:14:40 +0900

    d

--- main.py
+++ main.py
@@ -1,44 +1,62 @@
+from pygame.transform import scale
 from note import Note
 from osureader.reader import BeatmapParser
 from osureader.beatmap import Beatmap
-import os, pygame, sys
+import pygame as pg
+import os, sys
 
-reader = BeatmapParser()
 beatmap = Beatmap(reader.parse("D:/osu/Songs/481786 Levaslater - NNRT/Levaslater - NNRT (ZZHBOY) [CS' Another].osu"))
-objects = beatmap.hit_objects
 notes = list()
-for i in objects:
-    notes.append(Note().setNote(i))
-print(len(notes))
+reader = BeatmapParser()
+#objects = beatmap.hit_objects
+#for i in objects:
+#    notes.append(Note().setNote(i))
+#print(len(notes))
 #GG 
-notepng1 = pygame.image.load('mania-note1.png')
-noteobject = pygame.transform.scale(notepng1, (128, 100))
-screen = pygame.display.set_mode((600, 900))
-pygame.display.set_caption("Test")
-pygame.init()
 gametime = 0
-clock = pygame.time.Clock()
 running = True
 notepos = 0
-screen.fill((0, 0, 0))
+notepng1 = pg.image.load('mania-note1.png')
+notepng2 = pg.image.load('mania-note2.png')
+notepng3 = pg.image.load('mania-note2.png')
+notepng4 = pg.image.load('mania-note1.png')
+stageright = pg.image.load('mania-stage-right.png')
+stagehint = pg.image.load('mania-stage-hint.png')
+
+noteobject1 = pg.transform.scale(notepng1, (150, 50))
+noteobject2 = pg.transform.scale(notepng2, (150, 50))
+noteobject3 = pg.transform.scale(notepng3, (150, 50))
+noteobject4 = pg.transform.scale(notepng4, (150, 50))
+stageright = pg.transform.scale(stageright, (400, 2300))
+stagehint = pg.transform.scale(stagehint, (600, 100))
+screen = pg.display.set_mode((630, 1000))
+
+pg.display.set_caption("PYMAX")
+pg.init()
+clock = pg.time.Clock()
 
 # 메인
 while running:
     clock.tick(144)
-    gametime += clock.get_time() / 1000            #0부터 지금까지의 시간을 저장
+    gametime += clock.get_time() / 1000          #0부터 지금까지의 시간을 저장
     if gametime <= 2:
-        notepos = 0 + 900 * (gametime / 2)      #시간으로 노트 움직이기
+        notepos = 0 + 1050 * (gametime / 2)      #시간으로 노트 움직이기
         
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
             running = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
+        elif event.type == pg.MOUSEBUTTONDOWN:
             print(event)
-        elif event.type == pygame.MOUSEBUTTONUP:
+        elif event.type == pg.MOUSEBUTTONUP:
             print(event)
     screen.fill((0, 0, 0))
-    screen.blit(noteobject, (300, notepos))
-    pygame.display.update()
+    screen.blit(noteobject1, (0, notepos))
+    screen.blit(noteobject2, (150, notepos))
+    screen.blit(noteobject3, (300, notepos))
+    screen.blit(noteobject4, (450, notepos))
+    screen.blit(stageright, (600, 0))
+    screen.blit(stagehint, (0, 800))
+    pg.display.update()
 
-pygame.quit()
+pg.quit()
 sys.exit()
