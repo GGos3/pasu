0be7eb0ef8909577a7c6b1e94d4214704bc1a104
GGos3 <janpll0421@gmail.com>
2021-11-19 03:41:11 +0900

    게임 스코어 및 판정 이펙트 추가

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
+from pygame.constants import KEYDOWN
 from osureader.reader import BeatmapParser
 from osureader.beatmap import Beatmap
 from note import Note
 import maploader
 import pygame as pg
 import os
+import math
 import sys
 
-directory = 'songs/Gurenge/'
-fileName = "Gurenge [4K Lv.3].osu"
+directory = 'songs/Nhelv/'
+fileName = "Nhelv [4K Lv.4].osu"
 
-keypositions = maploader.Maploader().load_keyposition(directory+fileName)
-notelist = maploader.Maploader().load_notes(directory+fileName, keypositions)
+keypositions = maploader.Maploader().load_keyposition(directory + fileName)
+notelist = maploader.Maploader().load_notes(directory + fileName, keypositions)
 
 k1list = []  # 생성된 노트 리스트
 k2list = []  # 생성된 노트 리스트
@@ -18,7 +20,7 @@ k3list = []  # 생성된 노트 리스트
 k4list = []  # 생성된 노트 리스트
 
 gametime = 0  # 게임 플레이 시간
-exposure_time = 0 # 판정의 노출 시간
+exposure_time = 0  # 판정의 노출 시간
 running = True
 notepng1 = pg.image.load('skin/mania-note1.png')  # 노트 리소스
 notepng2 = pg.image.load('skin/mania-note2.png')  # 노트 리소스
@@ -27,22 +29,33 @@ notepng4 = pg.image.load('skin/mania-note1.png')  # 노트 리소스
 stageright = pg.image.load('skin/mania-stage-right.png')  # UI 리소스
 stageleft = pg.image.load('skin/mania-stage-left.png')  # UI 리소스
 stagehint = pg.image.load('skin/mania-stage-hint.png')  # 판정선 리소스
-hit0 = pg.image.load('skin/mania-hit0.png') # hit 0 리소스
-hit50 = pg.image.load('skin/mania-hit50.png') # hit 50 리소스
-hit100 = pg.image.load('skin/mania-hit100.png') # hit 100 리소스
-hit200 = pg.image.load('skin/mania-hit200.png') # hit 200 리소스
-hit300 = pg.image.load('skin/mania-hit300.png') # hit 300 리소스
-hit300g = pg.image.load('skin/mania-hit300g-0.png') # hit 300g 리소스
+hit0 = pg.image.load('skin/mania-hit0.png')  # hit 0 리소스
+hit50 = pg.image.load('skin/mania-hit50.png')  # hit 50 리소스
+hit100 = pg.image.load('skin/mania-hit100.png')  # hit 100 리소스
+hit200 = pg.image.load('skin/mania-hit200.png')  # hit 200 리소스
+hit300 = pg.image.load('skin/mania-hit300.png')  # hit 300 리소스
+hit300g = pg.image.load('skin/mania-hit300g-0.png')  # hit 300g 리소스
 
+score0 = pg.image.load('skin/score0.png')  # 숫자 이미지
+score1 = pg.image.load('skin/score0.png')  # 숫자 이미지
+score2 = pg.image.load('skin/score0.png')  # 숫자 이미지
+score3 = pg.image.load('skin/score0.png')  # 숫자 이미지
+score4 = pg.image.load('skin/score0.png')  # 숫자 이미지
+score5 = pg.image.load('skin/score0.png')  # 숫자 이미지
+score6 = pg.image.load('skin/score0.png')  # 숫자 이미지
+score7 = pg.image.load('skin/score0.png')  # 숫자 이미지
+score8 = pg.image.load('skin/score0.png')  # 숫자 이미지
+score9 = pg.image.load('skin/score0.png')  # 숫자 이미지
 
 noteobject1 = pg.transform.scale(notepng1, (150, 50))  # 노트 오브젝트 생성
 noteobject2 = pg.transform.scale(notepng2, (150, 50))  # 노트 오브젝트 생성
 noteobject3 = pg.transform.scale(notepng3, (150, 50))  # 노트 오브젝트 생성
 noteobject4 = pg.transform.scale(notepng4, (150, 50))  # 노트 오브젝트 생성
-stageright = pg.transform.scale(stageright, (400, 100))  # UI 오브젝트 생성
+stageright = pg.transform.scale(stageright, (8, 1000))  # UI 오브젝트 생성
+stageleft = pg.transform.scale(stageleft, (8, 1000))  # UI 오브젝트 생성
 stagehint = pg.transform.scale(stagehint, (600, 100))  # 판정선 생성
-screen = pg.display.set_mode((630, 1000))  # 해상도 설정
 
+screen = pg.display.set_mode((608, 1000))  # 해상도 설정
 pg.display.set_caption("pasu!")  # 타이틀
 pg.init()  # 파이게임 초기화
 sound = pg.mixer.music.load(f"{directory}audio.mp3")  # 음악 리소스 불러오기
@@ -52,9 +65,40 @@ clock = pg.time.Clock()
 
 isPlay = False  # 게임 시작 체크
 
-perfectpos = 800 # 판정 선
-duration = 500  # 노트 이동속도 (*이동에 걸리는 시간)
+TotalNotes = len(notelist)
+score = 0  # 게임 점수
+MaxScore = 1000000  # 최대 점수
+last_hit = hit0  # 현재 판정
+Bonus = 100
+perfectpos = 800  # 판정 선
+duration = 400  # 노트 이동속도 (*이동에 걸리는 시간)
 pressedkey = [False, False, False, False]  # 키 입력 체크 (추후 사용)
+
+
+def EXscore(MaxScore, TotalNotes, Judgement):
+    global Bonus
+    Hit = ()
+    if Judgement == "MAX":
+        Hit = ("MAX", 320, 32, 2, 0)
+    elif Judgement == "300":
+        Hit = ("300", 300, 32, 1, 0)
+    elif Judgement == "200":
+        Hit = ("200", 200, 16, 0, 8)
+    elif Judgement == "100":
+        Hit = ("100", 100, 8, 0, 24)
+    elif Judgement == "50":
+        Hit = ("50", 50, 4, 0, 44)
+    BaseScore = (MaxScore * 0.5 / TotalNotes) * (Hit[1] / 320)
+    Bonus = Bonus + Hit[3] - Hit[4]
+    if Bonus > 100:
+        Bonus = 100
+    elif Bonus < 0:
+        Bonus = 0
+    BonusScore = (MaxScore * 0.5 / TotalNotes) * \
+        (Hit[2] * math.sqrt(Bonus) / 320)
+    Score = BaseScore + BonusScore
+    return Score
+
 
 # 메인 루프
 while running:
@@ -74,6 +118,8 @@ while running:
             if not isPlay:
                 isPlay = True
                 pg.mixer.music.play()
+        #elif event.tpye == pg.KEYDOWN and event.key == pg.K_LCTRL and pg.K_r:
+            
 
         # 키보드 입력 부분
         elif event.type == pg.KEYDOWN and event.key == pg.K_z:  # 1번 키 (인덱스 0)
@@ -82,78 +128,142 @@ while running:
                 inputtime = gametime - k1list[0].starttime
                 if inputtime <= 16.5 and inputtime >= - 16.5:
                     print("MAX / ", inputtime)
+                    last_hit = hit300g
+                    exposure_time = 500
+                    score += (EXscore(MaxScore, TotalNotes, "MAX"))
                     k1list.pop(0)
                 elif inputtime <= 37.5 and inputtime >= - 37.5:
                     print("300 / ", inputtime)
+                    last_hit = hit300
+                    exposure_time = 500
+                    score += (EXscore(MaxScore, TotalNotes, "300"))
                     k1list.pop(0)
                 elif inputtime <= 70.5 and inputtime >= - 70.5:
                     print("200 / ", inputtime)
+                    last_hit = hit200
+                    exposure_time = 500
+                    score += (EXscore(MaxScore, TotalNotes, "200"))
                     k1list.pop(0)
                 elif inputtime <= 100.5 and inputtime >= - 100.5:
                     print("100 / ", inputtime)
+                    last_hit = hit100
+                    exposure_time = 500
+                    score += (EXscore(MaxScore, TotalNotes, "100"))
                     k1list.pop(0)
                 elif inputtime <= 124.5 and inputtime >= - 124.5:
                     print("50 / ", inputtime)
+                    last_hit = hit50
+                    exposure_time = 500
+                    score += (EXscore(MaxScore, TotalNotes, "50"))
                     k1list.pop(0)
-
+            print(score)
         elif event.type == pg.KEYDOWN and event.key == pg.K_x:  # 2번 키 (인덱스 1)
             pressedkey[1] = True
             if len(k2list) != 0:  # 16.5 ms 내
                 inputtime = gametime - k2list[0].starttime
                 if inputtime <= 16.5 and inputtime >= - 16.5:
                     print("MAX / ", inputtime)
+                    last_hit = hit300g
+                    exposure_time = 500
+                    score += EXscore(MaxScore, TotalNotes, "MAX")
                     k2list.pop(0)
                 elif inputtime <= 37.5 and inputtime >= - 37.5:
                     print("300 / ", inputtime)
+                    last_hit = hit300
+                    exposure_time = 500
+                    score += (EXscore(MaxScore, TotalNotes, "300"))
                     k2list.pop(0)
                 elif inputtime <= 70.5 and inputtime >= - 70.5:
                     print("200 / ", inputtime)
+                    last_hit = hit200
+                    exposure_time = 500
+                    score += (EXscore(MaxScore, TotalNotes, "200"))
                     k2list.pop(0)
                 elif inputtime <= 100.5 and inputtime >= - 100.5:
                     print("100 / ", inputtime)
+                    last_hit = hit100
+                    exposure_time = 500
+                    score += (EXscore(MaxScore, TotalNotes, "100"))
                     k2list.pop(0)
                 elif inputtime <= 124.5 and inputtime >= - 124.5:
                     print("50 / ", inputtime)
+                    last_hit = hit50
+                    exposure_time = 500
+                    score += (EXscore(MaxScore, TotalNotes, "50"))
                     k2list.pop(0)
-        elif event.type == pg.KEYDOWN and event.key == pg.K_KP_2:  # 3번 키 (인덱스 2)
+            print(score)
+        # 3번 키 (인덱스 2)
+        elif event.type == pg.KEYDOWN and event.key == pg.K_KP_2:
             pressedkey[2] = True
             if len(k3list) != 0:  # 16.5 ms 내
                 inputtime = gametime - k3list[0].starttime
                 if inputtime <= 16.5 and inputtime >= - 16.5:
                     print("MAX / ", inputtime)
+                    last_hit = hit300g
+                    exposure_time = 500
+                    score += (EXscore(MaxScore, TotalNotes, "MAX"))
                     k3list.pop(0)
                 elif inputtime <= 37.5 and inputtime >= - 37.5:
                     print("300 / ", inputtime)
+                    last_hit = hit300
+                    exposure_time = 500
+                    score += (EXscore(MaxScore, TotalNotes, "300"))
                     k3list.pop(0)
                 elif inputtime <= 70.5 and inputtime >= - 70.5:
                     print("200 / ", inputtime)
+                    last_hit = hit200
+                    exposure_time = 500
+                    score += (EXscore(MaxScore, TotalNotes, "200"))
                     k3list.pop(0)
                 elif inputtime <= 100.5 and inputtime >= - 100.5:
                     print("100 / ", inputtime)
+                    last_hit = hit100
+                    exposure_time = 500
+                    score += (EXscore(MaxScore, TotalNotes, "100"))
                     k3list.pop(0)
                 elif inputtime <= 124.5 and inputtime >= - 124.5:
                     print("50 / ", inputtime)
+                    last_hit = hit50
+                    exposure_time = 500
+                    score += (EXscore(MaxScore, TotalNotes, "50"))
                     k3list.pop(0)
-        elif event.type == pg.KEYDOWN and event.key == pg.K_KP_3:  # 4번 키 (인덱스 3)
+            print(score)
+        # 4번 키 (인덱스 3)
+        elif event.type == pg.KEYDOWN and event.key == pg.K_KP_3:
             pressedkey[3] = True
             if len(k4list) != 0:  # 16.5 ms 내
                 inputtime = gametime - k4list[0].starttime
                 if inputtime <= 16.5 and inputtime >= - 16.5:
                     print("MAX / ", inputtime)
+                    last_hit = hit300g
+                    exposure_time = 500
+                    score += (EXscore(MaxScore, TotalNotes, "MAX"))
                     k4list.pop(0)
                 elif inputtime <= 37.5 and inputtime >= - 37.5:
                     print("300 / ", inputtime)
+                    last_hit = hit300
+                    exposure_time = 500
+                    score += (EXscore(MaxScore, TotalNotes, "300"))
                     k4list.pop(0)
                 elif inputtime <= 70.5 and inputtime >= - 70.5:
                     print("200 / ", inputtime)
+                    last_hit = hit200
+                    exposure_time = 500
+                    score += (EXscore(MaxScore, TotalNotes, "200"))
                     k4list.pop(0)
                 elif inputtime <= 100.5 and inputtime >= - 100.5:
                     print("100 / ", inputtime)
+                    last_hit = hit100
+                    exposure_time = 500
+                    score += (EXscore(MaxScore, TotalNotes, "100"))
                     k4list.pop(0)
                 elif inputtime <= 124.5 and inputtime >= - 124.5:
                     print("50 / ", inputtime)
+                    last_hit = hit50
+                    exposure_time = 500
+                    score += (EXscore(MaxScore, TotalNotes, "50"))
                     k4list.pop(0)
-
+            print(score)
         # 키를 눌렀다 뗐을때 (추후 롱노트에 사용)
         elif event.type == pg.KEYUP and event.key == pg.K_z:  # 1번 키 (인덱스 0)
             pressedkey[0] = False
@@ -182,17 +292,30 @@ while running:
                     k4list.append(notelist.pop(0))
 
         screen.fill((0, 0, 0))  # 화면 검은색으로 채우기
+        screen.blit(stagehint, (4, 800))  # 판정선 불러오기
         screen.blit(stageright, (600, 0))  # UI 불러오기
-        screen.blit(stagehint, (0, 800))  # 판정선 불러오기
+        screen.blit(stageleft, (0, 0))  # UI 불러오기
 
         if exposure_time > 0:
             exposure_time -= clock.get_time()
-            screen.blit(hit0, (170, 500))
+            if last_hit == hit300g:
+                screen.blit(last_hit, (185, 500))
+            if last_hit == hit300:
+                screen.blit(last_hit, (185, 500))
+            if last_hit == hit200:
+                screen.blit(last_hit, (230, 500))
+            if last_hit == hit100:
+                screen.blit(last_hit, (235, 500))
+            if last_hit == hit50:
+                screen.blit(last_hit, (235, 500))
+            if last_hit == hit0:
+                screen.blit(last_hit, (235, 500))
 
         for i in k1list:
             # 노트 미스 처리
             if gametime > i.starttime + 124.5:
                 print("MISS / ", gametime - i.starttime)
+                last_hit = hit0
                 exposure_time = 500
                 k1list.remove(i)
             else:
@@ -204,6 +327,7 @@ while running:
         for i in k2list:
             if gametime > i.starttime + 124.5:
                 print("MISS / ", gametime - i.starttime)
+                last_hit = hit0
                 exposure_time = 500
                 k2list.remove(i)
             else:
@@ -215,6 +339,7 @@ while running:
         for i in k3list:
             if gametime > i.starttime + 124.5:
                 print("MISS / ", gametime - i.starttime)
+                last_hit = hit0
                 exposure_time = 500
                 k3list.remove(i)
             else:
@@ -226,6 +351,7 @@ while running:
         for i in k4list:
             if gametime > i.starttime + 124.5:
                 print("MISS / ", gametime - i.starttime)
+                last_hit = hit0
                 exposure_time = 500
                 k4list.remove(i)
             else:
